cb474bad1e3ca3742d4989bf99dd785e451f271b
PagiY <pagiyap003@gmail.com>
2023-08-21 23:55:42 +0800

    Added middlewares

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,14 @@
 import express, { Express } from 'express';
+import auth from './routes/auth.js';
+import territories from './routes/territories.js';
 
 const app: Express = express();
 const PORT = 8000;
+
+// middleware declarations
+app.use(express.json());
+app.use('/api', auth);
+app.use('/api', territories);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
