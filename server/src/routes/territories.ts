031cf3acb71ccc633431c41a265a420ff8893818
PagiY <pagiyap003@gmail.com>
2023-08-21 23:54:37 +0800

    Separate routes for login and getting territories

---
+++ server/src/routes/territories.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from 'express';
+
+const territories = express.Router();
+
+territories.get('/territories', async (req: Request, res: Response) => {
+  
+  try {
+    
+    const response = await fetch('https://netzwelt-devtest.azurewebsites.net/Territories/All');
+    
+    const result = await response.json();
+
+    return res.status(200).json(result);
+
+  } catch (err) {
+    return res.status(500).json({error: 'Server error.'});
+  }
+  
+});
+
+export default territories;
