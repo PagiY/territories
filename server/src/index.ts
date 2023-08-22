afe74521db240be51f81c1eac2cd0865d83b5709
PagiY <pagiyap003@gmail.com>
2023-08-23 01:29:25 +0800

    Prod-ready server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,28 @@
 import express, { Express } from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 import auth from './routes/auth.js';
 import territories from './routes/territories.js';
 
 const app: Express = express();
-const PORT = 8000;
+
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, '../build')));
 
 // middleware declarations
 app.use(cors());
 app.use(express.json());
 app.use('/api', auth);
 app.use('/api', territories);
+
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
